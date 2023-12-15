bdf11f71e6235a0055488dc97944cba09aba970a
Ondrej Mirtes <ondrej@mirtes.cz>
2023-12-15 08:52:15 +0100

    Introduce AbstractVisitorTestCase

--- tests/Visitor/DowngradeReadonlyPropertyVisitorTest.php
+++ tests/Visitor/DowngradeReadonlyPropertyVisitorTest.php
@@ -2,18 +2,11 @@
 
 namespace SimpleDowngrader\Visitor;
 
-use PhpParser\Lexer\Emulative;
-use PhpParser\Node\Stmt;
-use PhpParser\NodeTraverser;
 use PhpParser\NodeVisitor;
-use PhpParser\NodeVisitor\CloningVisitor;
-use PhpParser\Parser\Php7;
 use PHPStan\PhpDocParser\Printer\Printer;
-use PHPUnit\Framework\TestCase;
-use SimpleDowngrader\Php\PhpPrinter;
 use SimpleDowngrader\PhpDoc\PhpDocEditor;
 
-class DowngradeReadonlyPropertyVisitorTest extends TestCase
+class DowngradeReadonlyPropertyVisitorTest extends AbstractVisitorTestCase
 {
 
 	protected function getVisitor(): NodeVisitor
@@ -21,7 +14,6 @@ class DowngradeReadonlyPropertyVisitorTest extends TestCase
 		return new DowngradeReadonlyPropertyVisitor(new PhpDocEditor(new Printer()));
 	}
 
-	/** @return iterable<array{string, string}> */
 	public function dataVisitor(): iterable
 	{
 		yield [
@@ -167,42 +159,6 @@ class SomeClass
 PHP
 ,
 		];
-	}
-
-	/**
-	 * @dataProvider dataVisitor
-	 */
-	public function testVisitor(string $codeBefore, string $codeAfter): void
-	{
-		$lexer = new Emulative([
-			'usedAttributes' => [
-				'comments',
-				'startLine', 'endLine',
-				'startTokenPos', 'endTokenPos',
-			],
-		]);
-		$parser = new Php7($lexer);
-
-		/** @var Stmt[] $oldStmts */
-		$oldStmts = $parser->parse($codeBefore);
-		$oldTokens = $lexer->getTokens();
-
-		$cloningTraverser = new NodeTraverser();
-		$cloningTraverser->addVisitor(new CloningVisitor());
-
-		/** @var Stmt[] $newStmts */
-		$newStmts = $cloningTraverser->traverse($oldStmts);
-
-		$traverser = new NodeTraverser();
-		$traverser->addVisitor($this->getVisitor());
-
-		/** @var Stmt[] $newStmts */
-		$newStmts = $traverser->traverse($newStmts);
-
-		$printer = new PhpPrinter();
-		$newCode = $printer->printFormatPreserving($newStmts, $oldStmts, $oldTokens);
-
-		$this->assertSame($codeAfter, $newCode);
 	}
 
 }
