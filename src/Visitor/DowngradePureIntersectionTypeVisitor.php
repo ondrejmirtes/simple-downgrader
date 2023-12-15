e1c022033bd40a422274ab8abf42f701adc885c0
Ondrej Mirtes <ondrej@mirtes.cz>
2023-12-15 09:06:55 +0100

    Bug reproduction

--- src/Visitor/DowngradePureIntersectionTypeVisitor.php
+++ src/Visitor/DowngradePureIntersectionTypeVisitor.php
@@ -2,15 +2,37 @@
 
 namespace SimpleDowngrader\Visitor;
 
+use PhpParser\Comment\Doc;
 use PhpParser\Node;
 use PhpParser\NodeVisitorAbstract;
+use SimpleDowngrader\PhpDoc\PhpDocEditor;
 
 class DowngradePureIntersectionTypeVisitor extends NodeVisitorAbstract
 {
 
+	/** @var PhpDocEditor */
+	private $phpDocEditor;
+
+	public function __construct(PhpDocEditor $phpDocEditor)
+	{
+		$this->phpDocEditor = $phpDocEditor;
+	}
+
 	public function enterNode(Node $node)
 	{
+		if ($node instanceof Node\Stmt\Property && $node->type instanceof Node\IntersectionType) {
+			$phpDoc = '/** */';
+			if ($node->getDocComment() !== null) {
+				$phpDoc = $node->getDocComment()->getText();
+			}
 
+			$node->type = null;
+			$node->setDocComment(new Doc(
+				$this->phpDocEditor->edit($phpDoc, function ($node) {
+
+				})
+			));
+		}
 	}
 
 }
