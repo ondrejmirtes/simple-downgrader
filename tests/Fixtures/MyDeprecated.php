c078beb3a532bb0b1f89c8829729e5ec68e734ca
Markus Staab <maggus.staab@googlemail.com>
2025-09-20 09:02:11 +0200

    Update MyDeprecated.php

--- tests/Fixtures/MyDeprecated.php
+++ tests/Fixtures/MyDeprecated.php
@@ -1,16 +1,20 @@
-<?php
+<?php declare(strict_types = 1);
 
 namespace SimpleDowngrader\Fixtures;
 
 use Attribute;
 
-#[Attribute(Attribute::TARGET_FUNCTION | Attribute::TARGET_METHOD | Attribute::TARGET_CLASS | Attribute::TARGET_CLASS_CONSTANT | Attribute::TARGET_PROPERTY | Attribute::TARGET_PARAMETER | Attribute::TARGET_CONSTANT)]
+#[Attribute(Attribute::TARGET_FUNCTION | Attribute::TARGET_METHOD | Attribute::TARGET_CLASS | Attribute::TARGET_CLASS_CONSTANT | Attribute::TARGET_PROPERTY | Attribute::TARGET_PARAMETER)]
 class MyDeprecated
 {
+
 	public ?string $message;
 
 	public ?string $since;
 
-	public function __construct(?string $message = null, ?string $since = null) { /* … */ }
+	public function __construct(?string $message = null, ?string $since = null)
+	{
+		/* … */
+	}
 
 }
